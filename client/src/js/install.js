10aff51e34188b8088e2b2b0cec3098b4f1bd934
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-16 10:52:58 -0400

    added install alert

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,11 +6,22 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.prompt("")
+    const message = window.confirm("Are you sure you want ot install?");
+
+    if (resonse === "yes" || "y") {
+        alert("Your code has been installed");
+    } else if (resonse === "no" || "n") {
+        alert("Nothing was installed")
+    } else {
+        alert("Please enter 'yes' or 'no'");
+    }
+
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => { });
