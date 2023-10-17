9382a2523a2c18daf57580b8dc5cb7f9b96833b6
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-17 17:26:33 -0400

    Added WebpackPwaManifest

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -12,16 +12,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
 
-    // const promptEvent = window.deferredPrompt;
+    const promptEvent = window.deferredPrompt;
 
-    // if (!promptEvent) {
-    //     return;
-    // }
+    if (!promptEvent) {
+        return;
+    }
 
-    // promptEvent.prompt();
-    // window.deferredPrompt = null;
-    // butInstall.classList.toggle('hidden', true);
-    // -------------------------
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+
     butInstall.setAttribute('disabled', true);
     butInstall.textContent = 'Installed!';
     console.log("The install button has been pressed");
